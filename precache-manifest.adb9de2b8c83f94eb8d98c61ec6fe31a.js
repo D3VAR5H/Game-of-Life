e85bc7f9eece885ca0681413efe797f73350747d
D3VAR5H <dev103042@gmail.com>
2020-07-10 01:44:47 +0530

    Updates

---
+++ precache-manifest.adb9de2b8c83f94eb8d98c61ec6fe31a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ffc648f50a29212234e2897c502513",
+    "url": "/Game-of-Life/index.html"
+  },
+  {
+    "revision": "383965a61da90f756ec3",
+    "url": "/Game-of-Life/static/css/main.4650eaed.chunk.css"
+  },
+  {
+    "revision": "ca4d08c8c30fc96717a7",
+    "url": "/Game-of-Life/static/js/2.038d2dae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Game-of-Life/static/js/2.038d2dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383965a61da90f756ec3",
+    "url": "/Game-of-Life/static/js/main.be355be1.chunk.js"
+  },
+  {
+    "revision": "b69e4e4a53de6d5ec91b",
+    "url": "/Game-of-Life/static/js/runtime-main.f46c60ba.js"
+  }
+]);
